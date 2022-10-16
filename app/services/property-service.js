ab51456314bcd7f035e60af1d1377e623d7788dc
Furkan MM <furkanmm@Furkans-MacBook-Air.local>
2022-10-16 21:06:31 +0400

    added Test Button to updates

--- app/services/property-service.js
+++ app/services/property-service.js
@@ -80,7 +80,7 @@ export class PropertyService {
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
         console.log('Updating updateRecord -> ',headers);
-        this.http.post('/makeOffer', JSON.stringify({ 'property__c': property.id, 'Client_Offer__c' : 2345 }), {headers: headers}).subscribe((res => {
+        this.http.post('/makeOffer', JSON.stringify({ 'property__c': property.id, 'Client_Offer__c' : Math.random() }), {headers: headers}).subscribe((res => {
             console.log('Logger hehe',res);
         }));
     }
