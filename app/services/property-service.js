89112388be51f55c293bd91a831b6dd3b8c9d524
Furkan MM <furkanmm@Furkans-MacBook-Air.local>
2022-10-16 16:13:52 +0400

    added Test Button to update sf kbbg

--- app/services/property-service.js
+++ app/services/property-service.js
@@ -79,6 +79,7 @@ export class PropertyService {
         console.log('Updating Record',property);
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
+        console.log('Updating updateRecord -> ',headers);
         return this.http.post('/makeOffer', JSON.stringify({ 'property__c': property.id, 'Client_Offer__c' : 2345 }), {headers: headers});
     }
 
