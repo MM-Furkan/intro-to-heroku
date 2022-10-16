fbb748b7f9f8786c2e6d769957894518be994738
Furkan MM <furkanmm@Furkans-MacBook-Air.local>
2022-10-16 17:26:18 +0400

    added Test Button to update vvmmsvk

--- server.js
+++ server.js
@@ -90,6 +90,7 @@ app.get('/broker/:sfid', function(req, res) {
 
 app.post('/makeOffer',(req,res) => {
   console.log('req body', req.body);
+  console.log('req body', 'UPDATE '+propertyTable+ ' SET Client_Offer__c = '+req.body.Client_Offer__c+' WHERE sfid = '+req.body.property__c+'');
   client.query('UPDATE '+propertyTable+ ' SET Client_Offer__c = '+req.body.Client_Offer__c+' WHERE sfid = '+req.body.property__c+'');
   console.log('make');
   res.json(data);
